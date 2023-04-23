764ddfebc9fd34fb59d778ddecfd2db3facd5c24
ManiBala <manikandanbala002@gmail.com>
2023-04-23 20:32:54 +0530

    Chunk oriented steps added

--- src/main/java/com/batch/config/BatchConfig.java
+++ src/main/java/com/batch/config/BatchConfig.java
@@ -10,37 +10,45 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 @Slf4j
 public class BatchConfig {
 
-    private final JobLauncher jobLauncher;
-    private final JobRepository jobRepository;
-    private final PlatformTransactionManager batchTransactionManager;
+    final JobRepository jobRepository;
+    final PlatformTransactionManager batchTransactionManager;
+    public static final Logger logger = LoggerFactory.getLogger(BatchConfig.class);
+    private static final int BATCH_SIZE = 3;
 
-    public BatchConfig(JobLauncher jobLauncher, JobRepository jobRepository, PlatformTransactionManager batchTransactionManager) {
-        this.jobLauncher = jobLauncher;
+    public BatchConfig(JobRepository jobRepository, PlatformTransactionManager batchTransactionManager) {
         this.jobRepository = jobRepository;
         this.batchTransactionManager = batchTransactionManager;
     }
 
-    public static final Logger logger = LoggerFactory.getLogger(BatchConfig.class);
-
+    /**
+     * Job which contains multiple steps
+     */
     @Bean
     public Job firstJob() {
         return new JobBuilder("first job", jobRepository)
                 .incrementer(new RunIdIncrementer())
-                .start(firstStep())
+                .start(chunkStep())
+                .next(taskletStep())
                 .build();
     }
 
     @Bean
-    public Step firstStep() {
+    public Step taskletStep () {
         return new StepBuilder("first step", jobRepository)
                 .tasklet((stepContribution, chunkContext) -> {
                     logger.info("This is first tasklet step");
@@ -49,7 +57,29 @@ public class BatchConfig {
                 }, batchTransactionManager).build();
     }
 
+    @Bean
+    public Step chunkStep() {
+        return new StepBuilder("first step", jobRepository)
+                .<String, String>chunk(BATCH_SIZE, batchTransactionManager)
+                .reader(reader())
+                .writer(writer())
+                .build();
+    }
 
+    @Bean
+    public ItemReader<String> reader() {
+        List<String> data = Arrays.asList("Byte", "Code", "Data", "Disk", "File", "Input", "Loop", "Logic", "Mode", "Node");
+        return new ListItemReader<>(data);
+    }
 
+    @Bean
+    public ItemWriter<String> writer() {
+        return items -> {
+            for (String item : items) {
+                logger.info("Writing item: {}", item);
+            }
+            logger.info("------------ BATCH_SIZE: 3 documents written. ------------");
+        };
+    }
 
 }
